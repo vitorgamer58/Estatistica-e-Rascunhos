1b1d0546a4741b17457a092f1acfab5808cb7865
Vitor Araujo <vitorgamer58@outlook.com>
2019-08-06 17:55:54 -0300

    Gráfico de linhas - Python

---
+++ gráficos/Python/Linhas/linhas.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot
+
+eixo_x = ['1', '2']
+eixo_y = [500, 900]
+
+matplotlib.pyplot.plot(eixo_x, eixo_y)
+#matplotlib.pyplot.ylim(500, 1000) - mexe nas escalas
+matplotlib.pyplot.show()
