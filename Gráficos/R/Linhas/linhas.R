7382d22ec4db66beaf7ec6e9b32bdf025171013c
Vitor Araujo <vitorgamer58@outlook.com>
2019-08-08 10:18:16 -0300

    Adicionado Linhas em R

---
+++ Gráficos/R/Linhas/linhas.R
@@ -0,0 +1,4 @@
+#Gráfico de linhas
+x <- 1:10     # cria os valores x para os gráficos
+y <- 1:10      # cria os valores y para os gráficos
+plot(x, y, type = "l", xlab="Eixo X", ylab="Eixo Y", main ="Title")
--- gráficos/Python/Linhas/linhas.py
+++ gráficos/Python/Linhas/linhas.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot
 eixo_x = ['1', '2']
 eixo_y = [500, 900]
 
+matplotlib.pyplot.title('Titulo qualquer')
 matplotlib.pyplot.plot(eixo_x, eixo_y)
 #matplotlib.pyplot.ylim(500, 1000) - mexe nas escalas
+matplotlib.pyplot.xlabel('Legenda X')
+matplotlib.pyplot.ylabel('Legenda Y')
 matplotlib.pyplot.show()
